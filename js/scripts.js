cf36ccf43192506c4fb24abb26881c9e44a19b10
mandyfrei <afrei1@engr-131-11.eastern.ewu.edu>
2013-06-06 12:17:39 -0700

    Update weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
   
-	// Get the Weather
-	$.simpleWeather({
+  //Get Weather Form
+  $('button').click(function() {
+	  
+	  //Gets and Stores Entered Zipcode 
+	  var zip = $('input').val();
+	  console.log(zip);
+	  
+	  //Get the Weather
+	  $.simpleWeather({
 	
-		// Minimum properties to set
-		zipcode: '99004',
+	  // Minimum properties to set
+		zipcode: zip,
 		unit: 'f',
 		
-		// Method to display weather
+	  // Method to display weather
 		success: function(weather) {
 			
 			// Concatenate HTML code & weather variables.
 			// Put into one big'ol collection
-			html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+			
+			html = '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+			html += '<img src="img/' + weather.code + '.png "/>';
 			html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
 			html += '<li class="currently">'+weather.currently+'</li>';
-			html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
 			
 			// Write collection to page with html() method
 			// Note: difference between html() method vs. html option 
@@ -26,8 +34,9 @@ $(document).ready(function() {
 		error: function(error) {
 			$("#weather").html('<p>'+error+'</p>');
 		}
+	  });
 
-	});
-	// end getting Weather
-  
 });	
+   });
+  
+	
