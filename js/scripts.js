6d91b3a5e23f8bebcb7cadb987a0b4b45caf4aa5
mandyfrei <mandy.frei@eagles.ewu.edu>
2013-06-10 12:04:18 -0700

    FINAL UPLOAD

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   
+  $('.alert').hide();
+  
+  //Pressing Enter to Get Weather
+  //$('input').keyup(function() {
+	//  var value= $('input').val();
+	  //$('p').text(value);
+  //});
+  
   //Get Weather Form
   $('button').click(function() {
-	  
+		  
 	  //Gets and Stores Entered Zipcode 
 	  var zip = $('input').val();
 	  console.log(zip);
@@ -20,23 +28,36 @@ $(document).ready(function() {
 			// Concatenate HTML code & weather variables.
 			// Put into one big'ol collection
 			
-			html = '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-			html += '<img src="img/' + weather.code + '.png "/>';
-			html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-			html += '<li class="currently">'+weather.currently+'</li>';
+			html = '<h2 class="span6">' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+			html += '<img class="span5" src="img/' + weather.code + '.png "/>';
+			html += '<p class="span6">'+weather.city+', '+weather.region+'</p>';
+			html += '<p class="span6">' +weather.updated+'</p>';
+			html += '<a class="span6" href="'+weather.link+'">Check out the rest of the week &raquo;</a>';
+		
+				
 			
 			// Write collection to page with html() method
 			// Note: difference between html() method vs. html option 
 			$("#weather").html(html);
 		},
+		
+			// Method 
+			error: function(error) {
+				("#weather").text('hi');
+			}
+	
+		});
+		// end getting Weather
+		
+	});
 
-		// Method 
-		error: function(error) {
-			$("#weather").html('<p>'+error+'</p>');
-		}
-	  });
 
+  
 });	
-   });
+
+
+
+
+
   
 	
